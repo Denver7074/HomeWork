3ab0ccf28cb130a7e79b952dcfbc47786bbc6f43
Denver7074 <denver7074@yandex.ru>
2023-05-03 14:22:57 +0300

    Swagger

--- src/main/java/com/example/demo/controller/JournalController.java
+++ src/main/java/com/example/demo/controller/JournalController.java
@@ -2,7 +2,6 @@ package com.example.demo.controller;
 
 import com.example.demo.entities.Journal;
 import com.example.demo.services.JournalService;
-import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,25 +15,21 @@ public class JournalController {
     JournalService journalService;
 
     @GetMapping("/")
-    @ApiOperation("Просмотреть весь список журналов")
     public List<Journal> getAllJournals() {
         return journalService.findAll();
     }
 
     @GetMapping("/{id}")
-    @ApiOperation("Посмотреть конкретный журнал")
     public Journal getJournalById(@PathVariable Long id) {
         return journalService.findJournal(id);
     }
 
     @PostMapping("/")
-    @ApiOperation("Создать новый журнал")
     public void createJournal(@RequestBody String title, @RequestBody String description){
         journalService.saveJournal(title,description);
     }
 
     @DeleteMapping("/{id}")
-    @ApiOperation("Удалить журнал")
     public void deleteJournal(@PathVariable Long id){
         journalService.deleteJournal(id);
     }
